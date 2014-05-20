39580bc036759215a5bf09db2ceef555c75a84b4
Alex Voerman <alex.voerman@jda.com>
2014-05-20 10:56:37 -0500

    Adding kenpom data with output

--- server.js
+++ server.js
@@ -4,9 +4,8 @@ var request = require('request');
 var cheerio = require('cheerio');
 var app = express();
 
+/* We're grabbing metrics from rpiforecast. This will also grab the conference and record. */
 app.get('/rpidata', function(req, res) {
-
-    //we're only grabbing rpi forecast's data at this point
     url = 'http://www.rpiforecast.com/live-rpi.html';
 
     request(url, function(err, response, html) {
@@ -42,6 +41,42 @@ app.get('/rpidata', function(req, res) {
         }
     });
 });
+
+/* We have to keep these as separate requests because of school name differences */
+app.get('/kenpomdata', function(req, res) {
+
+    url = 'http://kenpom.com/';
+
+    request(url, function(err, response, html) {
+        if (!err) {
+            var $ = cheerio.load(html);
+            var team;
+            var teams = [];
+
+            $('tbody tr').each(function(i, el) {
+                var tr = $(this);
+                var tds = tr.children();
+
+                //we have to use the <td> position since theres no classes on the table
+                //also, get the team name from the <a> tag because seeds will appear next to team
+                team = {
+                    kpom: parseInt(tds.eq(0).text()),
+                    name: tds.children().eq(0).text()
+                };
+                teams.push(team);
+            });
+
+            //write the data to a json file
+            fs.writeFile('data/kenpomdata.json', JSON.stringify(teams, null, 0), function(err) {
+                if (err) console.log('Error writing file');
+            });
+
+            //just return the data I guess
+            res.send(teams);
+        }
+    });
+});
+
 
 app.listen('3001');
 
