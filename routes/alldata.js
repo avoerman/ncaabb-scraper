999374b4fffa6f6bc26a15dee75e8731230004a5
Alex Voerman <avoerman89@gmail.com>
2014-05-24 11:08:41 -0500

    Adding bracket matrix data to the all data request. Dont have all the aliases for the team names in until they appear in the sites grid

--- routes/alldata.js
+++ routes/alldata.js
@@ -5,6 +5,7 @@ var allteams = './data/allteams.json';
 var bpidata = './data/bpidata.json';
 var rpidata = './data/rpidata.json';
 var kenpomdata = './data/kenpomdata.json';
+var bracketmatrixdata = './data/bracketmatrixdata.json';
 
 module.exports = {
     '/alldata': function(req, res) {
@@ -19,18 +20,21 @@ module.exports = {
             var bpiteams = JSON.parse(fs.readFileSync(bpidata, 'utf8'));
             var rpiteams = JSON.parse(fs.readFileSync(rpidata, 'utf8'));
             var kpomteams = JSON.parse(fs.readFileSync(kenpomdata, 'utf8'));
+            var bmteams = JSON.parse(fs.readFileSync(bracketmatrixdata, 'utf8'));
 
             newteams.forEach(function(team) {
 
                 var bpiteam = _.findWhere(bpiteams, { name: team.bpi_name} );
                 var rpiteam = _.findWhere(rpiteams, { name: team.name} );
                 var kpomteam = _.findWhere(kpomteams, { name: team.kpom_name} );
+                var bmteam = (team.bm_name) ? _.findWhere(bmteams, { name: team.bm_name} ) : null;
 
                 var newteam = {
                     name: team.name,
                     rpi: (rpiteam ? rpiteam.rpi : null),
                     bpi: (bpiteam ? bpiteam.bpi : null),
-                    kpom: (kpomteam ? kpomteam.kpom : null)
+                    kpom: (kpomteam ? kpomteam.kpom : null),
+                    bm_seed: (bmteam ? bmteam.bm_seed : null)
                 };
                 outputteams.push(newteam);
             });
