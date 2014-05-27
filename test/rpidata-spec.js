72ad5739ad762e8888ec954bcd0057796de2e33e
Alex Voerman <alex.voerman@jda.com>
2014-05-27 13:13:50 -0500

    Cleaning test a bit. Adding length check.

--- test/rpidata-spec.js
+++ test/rpidata-spec.js
@@ -1,12 +1,12 @@
 var request = require('request');
 
 describe('alldata', function(abc) {
-    it('should respond with hello world', function(done) {
+    it('should test the correct teams were scraped', function(done) {
         request('http://localhost:3000/rpidata', function(error, response, body) {
             var teams = JSON.parse(body);
             var firstTeam = teams[0];
             var lastTeam = teams[348];
-
+            expect(teams.length).toEqual(349);
             expect(firstTeam.name).toEqual('Florida');
             expect(lastTeam.name).toEqual('Presbyterian');
             done();
