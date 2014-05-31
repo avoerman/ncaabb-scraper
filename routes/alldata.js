5a6aab8114276414965fff584e65a0f91218e795
Alex Voerman <avoerman89@gmail.com>
2014-05-30 22:35:21 -0500

    Big update. Adding asynchronous parallel requests to scrape all data. No longer loading assumed .json files

--- routes/alldata.js
+++ routes/alldata.js
@@ -1,61 +1,77 @@
-var dumptojson = require('../lib/dump-to-json.js');
 var fs = require('fs');
 var _ = require('underscore');
+var rpiscraper = require('../lib/scrapers/rpidata.js');
+var bpiscraper = require('../lib/scrapers/bpidata.js');
+var kpomscraper = require('../lib/scrapers/kenpomdata.js');
+var bmscraper = require('../lib/scrapers/bracketmatrixdata.js');
+var dumptojson = require('../lib/dump-to-json.js');
+
+var async = require('async');
 
 module.exports = {
     '/alldata': function(req, res) {
+        async.parallel([
 
-        var allteams = './data/input/allteams.json';
+            function(callback) {
+                rpiscraper.scrape(null, callback);
+            },
+            function(callback) {
+                bpiscraper.scrape(null, callback);
+            },
+            function(callback) {
+                kpomscraper.scrape(null, callback);
+            },
+            function(callback) {
+                bmscraper.scrape(null, callback);
+            }
+        ], function(err, result) {
+            var allteams = './data/input/allteams.json';
 
-        var bpidata = './data/bpidata.json';
-        var rpidata = './data/rpidata.json';
-        var kenpomdata = './data/kenpomdata.json';
-        var bracketmatrixdata = './data/bracketmatrixdata.json';
+            fs.readFile(allteams, 'utf8', function(err, data) {
+                if (err) throw err;
 
-        fs.readFile(allteams, 'utf8', function(err, data) {
-            if (err) throw err;
+                var newteams = JSON.parse(data);
+                var outputteams = [];
 
-            var newteams = JSON.parse(data);
-            var outputteams = [];
+                var rpiteams = result[0];
+                var bpiteams = result[1];
+                var kpomteams = result[2];
+                var bmteams = result[3];
 
-            //For now, we'll just import the existing json files. In future these should be requests.
-            var bpiteams = JSON.parse(fs.readFileSync(bpidata, 'utf8'));
-            var rpiteams = JSON.parse(fs.readFileSync(rpidata, 'utf8'));
-            var kpomteams = JSON.parse(fs.readFileSync(kenpomdata, 'utf8'));
-            var bmteams = JSON.parse(fs.readFileSync(bracketmatrixdata, 'utf8'));
+                newteams.forEach(function(team) {
 
-            newteams.forEach(function(team) {
+                    var bpiteam = _.findWhere(bpiteams, {
+                        name: team.bpi_name
+                    });
+                    var rpiteam = _.findWhere(rpiteams, {
+                        name: team.name
+                    });
+                    var kpomteam = _.findWhere(kpomteams, {
+                        name: team.kpom_name
+                    });
+                    var bmteam = (team.bm_name) ? _.findWhere(bmteams, {
+                        name: team.bm_name
+                    }) : null;
 
-                var bpiteam = _.findWhere(bpiteams, {
-                    name: team.bpi_name
+                    var newteam = {
+                        name: team.name,
+                        rpi: (rpiteam ? rpiteam.rpi : null),
+                        bpi: (bpiteam ? bpiteam.bpi : null),
+                        kpom: (kpomteam ? kpomteam.kpom : null),
+                        bm_seed: (bmteam ? bmteam.bm_seed : null),
+                        bm_avgseed: (bmteam ? bmteam.bm_avgseed : null),
+                        bm_numbrackets: (bmteam ? bmteam.bm_numbrackets : null)
+                    };
+
+                    outputteams.push(newteam);
                 });
-                var rpiteam = _.findWhere(rpiteams, {
-                    name: team.name
-                });
-                var kpomteam = _.findWhere(kpomteams, {
-                    name: team.kpom_name
-                });
-                var bmteam = (team.bm_name) ? _.findWhere(bmteams, {
-                    name: team.bm_name
-                }) : null;
 
-                var newteam = {
-                    name: team.name,
-                    rpi: (rpiteam ? rpiteam.rpi : null),
-                    bpi: (bpiteam ? bpiteam.bpi : null),
-                    kpom: (kpomteam ? kpomteam.kpom : null),
-                    bm_seed: (bmteam ? bmteam.bm_seed : null),
-                    bm_avgseed: (bmteam ? bmteam.bm_avgseed : null),
-                    bm_numbrackets: (bmteam ? bmteam.bm_numbrackets : null)
-                };
+                outputteams = sortByRPIBPIKPOMAverage(outputteams);
 
-                outputteams.push(newteam);
+                dumptojson.dump(outputteams, 'alldata.json');
+                res.send(outputteams);
             });
 
-            outputteams = sortByRPIBPIKPOMAverage(outputteams);
-
-            dumptojson.dump(outputteams, 'alldata.json');
-            res.send(outputteams);
         });
     }
 };
